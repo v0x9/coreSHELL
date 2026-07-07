56b939776957c3d7f900e8db91ef556a64aef50b
v0x9 <aman23078@iiitd.ac.in>
2026-07-08 05:18:12 +0530

    added temp auth

--- backend/src/auth/jwt.ts
+++ backend/src/auth/jwt.ts
@@ -1,1 +1,57 @@
 // Empty file for user implementation
+
+import express from "express";
+import http from "http";
+import {Server} from "socket.io";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const app = express();
+
+//Middleware
+
+app.use(cors());
+app.use(express.json());
+
+//health check 
+
+app.get("/health" , (req , res)=>{
+    res.status(200).json({
+        status:"OK" , 
+        message:"coreSHell backend is running"
+    });
+});
+
+//creating a http server
+
+const server = http.createServer(app);
+
+//attaching socket io
+
+const io = new Server(server ,{
+    cors:{
+        origin: "*"
+    }
+});
+
+//temporary socket handler 
+
+io.on("connection", (socket)=>{
+    console.log(`Client Connected ${socket.id}`);
+
+    socket.on("disconnect"  , ()=>{
+        console.log(`CLient disconnected ${socket.id}`);
+    });
+})
+
+
+const PORT = process.env.PORT || 3000;
+
+
+server.listen(PORT , ()=>{
+    console.log(`Server listening on port ${PORT}`)
+});
+
